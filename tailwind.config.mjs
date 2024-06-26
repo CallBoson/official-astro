98fe8ec3b072a70558531ab03ec62b6916f9e5b9
丢那妈 <9441896+bosons@user.noreply.gitee.com>
2024-06-26 16:25:12 +0800

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3F53FF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
