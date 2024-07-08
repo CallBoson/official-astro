37bd73a7695eb0b4219f69f4504428ddbd6360fc
丢那妈 <9441896+bosons@user.noreply.gitee.com>
2024-07-08 16:31:38 +0800

    隐私协议

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,10 +1,9 @@
 export const toast = (content) => {
   const dom = document.createElement("div");
+  const toastClass =
+    "min-w-[250px] bg-[rgba(0,0,0,.8)] text-white text-center rounded p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[99]";
   document.body.appendChild(dom);
-  dom.setAttribute(
-    "class",
-    "min-w-[250px] bg-[rgba(0,0,0,.8)] text-white text-center rounded p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[99]"
-  );
+  dom.setAttribute("class", toastClass);
   dom.innerHTML = content;
   setTimeout(() => {
     dom.remove();
