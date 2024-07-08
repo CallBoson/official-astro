bdf81fd4ea4ac0802fd3e4283798cd94964b0974
丢那妈 <9441896+bosons@user.noreply.gitee.com>
2024-07-08 15:50:47 +0800

    toast

---
+++ src/utils/toast.js
@@ -0,0 +1,12 @@
+export const toast = (content) => {
+  const dom = document.createElement("div");
+  document.body.appendChild(dom);
+  dom.setAttribute(
+    "class",
+    "min-w-[250px] bg-[rgba(0,0,0,.8)] text-white text-center rounded p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+  );
+  dom.innerHTML = content;
+  setTimeout(() => {
+    dom.remove();
+  }, 1500);
+};
