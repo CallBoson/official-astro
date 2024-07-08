07bd8ba8395d8af5c36d53db4d17b74709aa4efc
丢那妈 <9441896+bosons@user.noreply.gitee.com>
2024-07-08 16:07:21 +0800

    职位详情页

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -3,7 +3,7 @@ export const toast = (content) => {
   document.body.appendChild(dom);
   dom.setAttribute(
     "class",
-    "min-w-[250px] bg-[rgba(0,0,0,.8)] text-white text-center rounded p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+    "min-w-[250px] bg-[rgba(0,0,0,.8)] text-white text-center rounded p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[99]"
   );
   dom.innerHTML = content;
   setTimeout(() => {
